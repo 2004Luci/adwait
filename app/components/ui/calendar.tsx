5b3b18de86d3a1f072298ad0f0d708b07ca5694d
Mit <mit4sheth@gmail.com>
2026-01-13 19:09:42 +0530

    Update calendar.tsx

--- app/components/ui/calendar.tsx
+++ app/components/ui/calendar.tsx
@@ -26,11 +26,11 @@ function Calendar({
         nav: "flex items-center gap-1",
         button_previous: cn(
           buttonVariants({ variant: "outline" }),
-          "size-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1"
+          "size-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute top-4 left-4 z-50"
         ),
         button_next: cn(
           buttonVariants({ variant: "outline" }),
-          "size-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1"
+          "size-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute top-4 right-4 z-50"
         ),
         // Grid
         month_grid: "w-full border-collapse space-x-1",
